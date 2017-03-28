7ec58ab8e056f7507d55ce5f9661e4482bae2cdf
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-03-28 11:53:15 +0000

    first

---
+++ app.py
@@ -0,0 +1,50 @@
+import json
+import urllib
+import os
+import os.path
+import sys
+import logging
+from flask import Flask
+from flask import render_template
+from flask import request, url_for, make_response
+from watson_developer_cloud import ConversationV1
+
+conversation = ConversationV1(
+    username='81cae901-ee0e-4066-b333-c6d9cc5532ec',
+    password='NCdy2rD8GQ5N',
+    version='2016-09-20')
+
+workspace_id = 'e5fa2b42-e839-4e1b-9c6d-4d3ca9a93330'
+
+app = Flask(__name__, static_url_path='/static')
+@app.route("/", methods=['GET', 'POST'])
+
+def main_page():
+
+	if request.method == 'GET':
+		return render_template("index2.html")	
+
+	elif request.method == 'POST':
+		#print("inside post1")
+		response = conversation.message(workspace_id=workspace_id, message_input={'text': request.form['message']})
+		#print(response)
+		#print("inside post2")
+		print(json.dumps(response, indent=2))
+		#print("inside post3")
+		#response_message = response.read();
+		#print("inside post4")
+		#response_message = json.dumps(response, indent=2)
+		#print("inside post5")
+		#res = json.load(response_message)
+		#print(type(response))
+		#print(str(response['output']['text'][0]))
+		return str(response['output']['text'][0])
+		
+
+if __name__ == "__main__":
+	port = int(os.getenv('PORT', 5000))
+	print "Starting app on port %d" % port
+	app.run(debug=True, port=port, host='0.0.0.0')
+	
+	
+
