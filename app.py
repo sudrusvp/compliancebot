425af6911a3b34d8e463c98f627b0f0b353f04a0
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-03-29 13:34:00 +0000

    fifth

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def main_page():
 		#print(response)
 		#print("inside post2")
 		print(json.dumps(response, indent=2))
+		print("***********")
+		print str(response['context'])
 		#print("inside post3")
 		#response_message = response.read();
 		#print("inside post4")
