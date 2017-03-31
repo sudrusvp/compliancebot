44adcc843fc734346600e51186086b0bce67ef05
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-03-31 11:45:18 +0000

    fifth

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ import logging
 from flask import Flask
 from flask import render_template
 from flask import request, url_for, make_response
+from dotenv import load_dotenv, find_dotenv
 from watson_developer_cloud import ConversationV1
+from watson_developer_cloud import ToneAnalyzerV3
+import tone_detection
+load_dotenv(find_dotenv())
 
 conversation = ConversationV1(
     username='81cae901-ee0e-4066-b333-c6d9cc5532ec',
     password='NCdy2rD8GQ5N',
     version='2017-02-03')
 
-workspace_id = 'e5fa2b42-e839-4e1b-9c6d-4d3ca9a93330'
-context={}
+conv_workspace_id = 'e5fa2b42-e839-4e1b-9c6d-4d3ca9a93330'
+#context={
+
+tone_analyzer = ToneAnalyzerV3(
+    username = '20c2903e-48a9-4fd5-8f0b-5e699fa5343e',
+    password = 'ZC2WBeLbXUXs'
+    version = '2017-02-03')
+
+maintainToneHistoryInContext = True 
+
 app = Flask(__name__, static_url_path='/static')
 @app.route("/", methods=['GET', 'POST'])
 
@@ -25,8 +37,22 @@ def main_page():
 		return render_template("index2.html")	
 
 	elif request.method == 'POST':
-		response = conversation.message(workspace_id=workspace_id, message_input={'text': request.form['message']},context=context)
-		if(response[intents][0])
+		
+		payload = {
+    		'workspace_id': conv_workspace_id,
+ 	 		'input': {
+        		'text': request.form['message']
+    		}
+		}
+		
+		tone = tone_analyzer.tone(text=payload['input']['text'])
+		conversation_payload = tone_detection.\
+        	updateUserTone(payload, tone, maintainToneHistoryInContext)
+        	
+		response = conversation.message( workspace_id = conv_workspace_id,
+										 message_input=conversation_payload['input'],
+                                   		 context=conversation_payload['context'])
+		print(json.dumps(response,indents=2))
 		return str(response['output']['text'][0])
 		
 
