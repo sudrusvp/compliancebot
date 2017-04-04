bfa993bad6cacfbe0df61a4a19342387091d489f
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-04-04 14:42:25 +0000

    tues24

--- static/php/write.php
+++ static/php/write.php
@@ -1,9 +1,14 @@
+<html>
+<p>Hi</p>
 <?php
 function writeToFile() {
-    //$fHandle = fopen('static/js/test.txt', 'w');
-    //fwrite($fHandle, 'Hellloo');
-    //fclose($fHandle);
-    echo "Hi";
+	$myfile = fopen("static/php/test.txt", "w") or die("Unable to open file!");
+	$txt = "John Doe\n";
+	fwrite($myfile, $txt);
+	$txt = "Jane Doe\n";
+	fwrite($myfile, $txt);
+	fclose($myfile);
 }
 writeToFile();
 ?>
+</html>
