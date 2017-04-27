c667ec4be697adc37a3889fb0136a46d476c3685
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-27 13:30:30 +0000

    CONN DB

--- connect_db.py
+++ connect_db.py
@@ -3,8 +3,8 @@ from ibm_db import connect
 
 def connection():
 	try:
-#		conn = ibm_db.connect("DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
-		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
+		conn = ibm_db.connect("DRIVER={IBM DB2 ODBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;")
+#		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
 		print "Connection succeeded..........!!"
 	except:
 		print "connection unsuccessful.....!!"
