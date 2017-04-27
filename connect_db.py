8f5acbbf4fe52c239b593ecface72bbffe2780d9
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-27 10:11:50 +0000

    conn_db

--- connect_db.py
+++ connect_db.py
@@ -2,7 +2,7 @@ import ibm_db
 from ibm_db import connect
 
 def connection():
-	conn_str = "DRIVER={IBM DB2 JDBC};DATABASE=IGI_DB.IGACORE;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;"
+	conn_str = "DRIVER={IBM DB2 JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;"
 	conn = ibm_db.connect(conn_str, '', '')
 	if conn:
 		print "Connection succeeded."
