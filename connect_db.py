7c0d8de463538430868daee07cbf797fd9cbf1c8
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-27 10:53:14 +0000

    conn_db

--- connect_db.py
+++ connect_db.py
@@ -3,10 +3,10 @@ import jaydebeapi
 from ibm_db import connect
 
 def connection():
-#	conn = ibm_db.connect("DRIVER={IBM DB2 JDBC Universal Driver Architecture};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
-	conn = jaydebeapi.connect('com.ibm.db2.jcc.DB2Driver', ['jdbc:db2://10.51.227.228:50000/IGI_DB','igiinst','ideas'])
+	conn = ibm_db.connect("DRIVER={IBM DB2 JDBC Universal Driver Architecture};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
+#	conn = jaydebeapi.connect('com.ibm.db2.jcc.DB2Driver', ['jdbc:db2://10.51.227.228:50000/IGI_DB','igiinst','ideas'])
 	if conn:
 		print "Connection succeeded."
-#		ibm_db.close(conn)
+		ibm_db.close(conn)
 	else:
 		print "Connection failed."
