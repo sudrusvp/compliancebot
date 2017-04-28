9f6fd36456952b5b400afaf4cc7d344491f6afba
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-28 10:23:46 +0000

    test1

--- connect_db.py
+++ connect_db.py
@@ -5,7 +5,7 @@ from ibm_db import connect
 def connection():
 	try:
 #		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=itimdb;HOSTNAME=9.212.151.160;PORT=50002;PROTOCOL=TCPIP;UID=itimuser;PWD=this4now;","","")
-		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC CLI DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.226.227;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
+		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC CLI DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
 #		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
 #		conn = MySQLdb.Connection(host='localhost', port=3306,user='root', passwd='admin', db='test')
 		
