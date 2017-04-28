643c1b1c31171f7ae3834c0df8c11cd4114b4072
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-28 05:40:26 +0000

    day1

--- connect_db.py
+++ connect_db.py
@@ -1,11 +1,13 @@
 import ibm_db
-import logging
+import MySQLdb
 from ibm_db import connect
 
 def connection():
 	try:
-		conn = ibm_db.connect("DRIVER={IBM DB2 LUW JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
+#		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
 #		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
+		conn = ibm_db.connect("IGI_DB","10.51.227.228","50000","TCPIP","igiinst","ideas")
+		
 		print "Connection succeeded..........!!"
 	except:
 		print "connection unsuccessful.....!!"
