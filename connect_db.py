3234383e775c17f83286c883d44ce33b85134b78
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-28 06:33:13 +0000

    test4

--- connect_db.py
+++ connect_db.py
@@ -3,18 +3,17 @@ import MySQLdb
 from ibm_db import connect
 
 def connection():
-#	try:
+	try:
 #		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
 #		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
-	conn = MySQLdb.connect("localhost","root","admin","test" )
-#		conn=MySQLdb.connect(host=####, user=####, passwd=####, db=####)
+		conn = MySQLdb.Connection(host='127.0.0.1', port='3306',user='root', passwd='admin', db='test')
 		
-#		print "Connection succeeded..........!!"
-#	except:
-#		print "connection unsuccessful.....!!"
-##		print ibm_db.conn_errormsg()
-#	finally:
-#		try:
-#			conn.close()
-#		except:
-#			print "can not close conn......!!"
+		print "Connection succeeded..........!!"
+	except Exception as e:
+		print("connection unsuccessful.....!!  "+e)
+#		print ibm_db.conn_errormsg()
+	finally:
+		try:
+			conn.close()
+		except:
+			print "can not close conn......!!"
