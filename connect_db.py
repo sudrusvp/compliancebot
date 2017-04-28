880d65e04edb69cf9979995685131f9edf913584
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-28 08:35:59 +0000

    connect itim db

--- connect_db.py
+++ connect_db.py
@@ -4,9 +4,9 @@ from ibm_db import connect
 
 def connection():
 	try:
-#		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
+		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=itimdb;HOSTNAME=9.212.151.160;PORT=50002;PROTOCOL=TCPIP;UID=itimuser;PWD=this4now;","","")
 #		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
-		conn = MySQLdb.Connection(host='localhost', port=3306,user='root', passwd='admin', db='test')
+#		conn = MySQLdb.Connection(host='localhost', port=3306,user='root', passwd='admin', db='test')
 		
 		print "Connection succeeded..........!!"
 	except Exception as e:
@@ -14,6 +14,7 @@ def connection():
 #		print ibm_db.conn_errormsg()
 	finally:
 		try:
-			conn.close()
+#			conn.close()
+			ibm_db.close(conn)
 		except:
 			print "can not close conn......!!"
