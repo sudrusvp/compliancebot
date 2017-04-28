96c062571c5bd2e5e36f2ba7a52d59f3d4aa98be
sid panchal <sudarshanpanchal9@gmail.com>
2017-04-28 06:44:23 +0000

    integer req

--- connect_db.py
+++ connect_db.py
@@ -6,7 +6,7 @@ def connection():
 	try:
 #		conn = ibm_db.connect("DRIVER={IBM DB2 JDBC DRIVER};DATABASE=IGI_DB;HOSTNAME=10.51.227.228;PORT=50000;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;","","")
 #		conn = ibm_db.connect("dsn=jdbc:db2://10.51.227.228:50000/IGI_DB;PROTOCOL=TCPIP;UID=igiinst;PWD=ideas;", "","")
-		conn = MySQLdb.Connection(host='127.0.0.1', port='3306',user='root', passwd='admin', db='test')
+		conn = MySQLdb.Connection(host='127.0.0.1', port=3306,user='root', passwd='admin', db='test')
 		
 		print "Connection succeeded..........!!"
 	except Exception as e:
