6feb92e5f984e052ff85b72abd40518d1043b781
sid panchal <sudarshanpanchal9@gmail.com>
2017-05-12 10:12:22 +0000

    session2

--- app.py
+++ app.py
@@ -42,18 +42,18 @@ def main_page():
 		client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
 		client.connect()
 		session = client.session()
-		print(session)
+		print(json.dumps(session,indent=4)
 		my_db=client['employee_db']
 		for document in my_db:
 			print document
-		data = {
-			"_id" : "employee2",
-			"first_name" : "rakesh",
-			"last_name" : "patil"
-		}
-		my_doc1 = my_db.create_document(data)
-		if my_doc1.exists():
-			print "my_doc1 successfully created!!!"
+#		data = {
+#			"_id" : "employee2",
+#			"first_name" : "rakesh",
+#			"last_name" : "patil"
+#		}
+#		my_doc1 = my_db.create_document(data)
+#		if my_doc1.exists():
+#			print "my_doc1 successfully created!!!"
 		print(requests.get('http://api.ipify.org/?format=json').json())
 		return render_template("index2.html")
 		
